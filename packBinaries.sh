502de83382dce0882ad603aea7dc83aad8fdbd72
albertwcheng <albertwcheng@gmail.com>
2022-05-01 22:49:16 -0700

    c

---
+++ packBinaries.sh
@@ -0,0 +1,3 @@
+mkdir Binaries
+mkdir Binaries/$MACHTYPE
+cp JACKIE.countSeqNeighbors.pmulti JACKIE.encodeSeqCountDatabase JACKIE.encodeSeqSpace.prefixed JACKIE.encodeSeqSpace JACKIE.countSeqNeighbors JACKIE.encodeSeqSpaceNGG JACKIE.sortToBed JACKIE.bin `which JACKIE.queryDB` Binaries/$MACHTYPE
